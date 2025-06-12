0c30d27dea4ae9b66a7cbc86121d68c5363a324a
Siddhesh Shahu <104705049+Siddhesh-Shahu@users.noreply.github.com>
2025-06-12 21:18:42 +0530

    Create Kainess_CameraTests.swift

---
+++ Kainess CameraTests/Kainess_CameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kainess_CameraTests.swift
+//  Kainess CameraTests
+//
+//  Created by Siddhesh Shahu on 11/06/25.
+//
+
+import Testing
+@testable import Kainess_Camera
+
+struct Kainess_CameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
