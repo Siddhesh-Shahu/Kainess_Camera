ef1baa76de865a461a387db2732226f78e4b9f12
Siddhesh Shahu <104705049+Siddhesh-Shahu@users.noreply.github.com>
2025-06-12 21:18:40 +0530

    Create Kainess_CameraApp.swift

---
+++ Kainess Camera/Kainess_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kainess_CameraApp.swift
+//  Kainess Camera
+//
+//  Created by Siddhesh Shahu on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Kainess_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
